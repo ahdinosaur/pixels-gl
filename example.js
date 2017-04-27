b61b9fe52506f739bb8f62b525aba5145862c792
Michael Williams <michael.williams@enspiral.com>
2017-04-27 23:40:40 +1200

    does things

--- example.js
+++ example.js
@@ -1,15 +1,42 @@
 const h = require('react-hyperscript')
 const { render } = require('react-dom')
+const { NearestCopy, LinearCopy } = require('gl-react')
 const { Surface } = require('gl-react-dom')
 const rainbowPixels = require('rainbow-pixels')
 const pull = require('pull-stream')
+const convert = require('ndpixels-convert')
+const Ndarray = require('ndarray')
+const insertCss = require('insert-css')
 const pixelsGl = require('./')
 
 const main = document.createElement('div')
+main.className = 'main'
 document.body.appendChild(main)
 
+insertCss(`
+  html, body, .main, canvas {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }   
+`)
+
+const size = {
+  width: document.body.clientWidth,
+  height: document.body.clientHeight,
+}
+
+console.log('size', size)
+
 pull(
-  pull.infinite(rainbowPixels()),
+  pull.infinite(rainbowPixels({
+    shape: [size.width / 4, size.height / 4]
+  })),
+  pull.map(convert('hsl', 'rgb')),
+  pull.map(pixels => {
+    return Ndarray(Uint8Array.from(pixels.data), pixels.shape, pixels.stride)
+  }),
   pull.asyncMap((pixels, cb) => {
     window.requestAnimationFrame(() => {
       cb(null, pixels)
@@ -17,7 +44,7 @@ pull(
   }),
   pull.drain(pixels => {
     render(
-      h(Surface, { width: 300, height: 300 }, [
+      h(Surface, size, [
         h(pixelsGl, { pixels })
       ]),
       main
--- index.js
+++ index.js
@@ -4,17 +4,29 @@ const { Shaders, Node, GLSL } = require('gl-react')
 module.exports = pixelsGl
 
 const shaders = Shaders.create({
-  helloGL: {
+  pixels: {
     frag: GLSL`
-precision highp float;
-varying vec2 uv;
-void main() {
-  gl_FragColor = vec4(uv.x, uv.y, 0.5, 1.0);
-}
-`
+      precision highp float;
+      varying vec2 uv;
+      uniform sampler2D pixels;
+      void main() {
+        gl_FragColor = texture2D(pixels, uv);
+      }
+    `
   }
 })
 
 function pixelsGl ({ pixels }) {
-  return h(Node, { shader: shaders.helloGL })
+  return h(Node, {
+    shader: shaders.pixels,
+    uniforms: {
+      pixels
+    },
+    uniformsOptions: {
+      pixels: {
+        interpolation: 'nearest',
+        wrap: ['clamp to edge', 'clamp to edge']
+      }
+    }
+  })
 }
