91c60a37e9403f6000ef43f5b43904aadcffebba
Michael Williams <michael.williams@enspiral.com>
2017-05-30 01:27:15 +1200

    bring back the pipelines

--- example.js
+++ example.js
@@ -1,9 +1,8 @@
 const rainbowPixels = require('rainbow-pixels')
-const pull = require('pull-stream')
 const convert = require('ndpixels-convert')
 const Ndarray = require('ndarray')
 const insertCss = require('insert-css')
-const pullRaf = require('pull-raf')
+const pipe = require('value-pipe')
 const Regl = require('regl')
 const PixelsGl = require('./')
 
@@ -14,16 +13,16 @@ const size = {
 const regl = Regl()
 const pixelsGl = PixelsGl(regl)
 
-const generatePixels = rainbowPixels({
-  shape: [size.width / 4, size.height / 4]
-})
-const hslToRgb = convert('hsl', 'rgb')
+const createPixels = pipe(
+  rainbowPixels({
+    shape: [size.width / 4, size.height / 4]
+  }),
+  convert('hsl', 'rgb'),
+  castToUint8
+)
 
 regl.frame(({ time }) => {
-  const nextPixels = generatePixels()
-  const rgbPixels = hslToRgb(nextPixels)
-  const uint8Pixels = castToUint8(rgbPixels)
-  pixelsGl({ pixels: uint8Pixels })
+  pixelsGl({ pixels: createPixels() })
 })
 
 function castToUint8 (pixels) {
